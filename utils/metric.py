799e9296e616e2eed79fceb653419ca9ec84f175
reallsp <228259802@qq.com>
2022-04-09 12:00:28 +0800

    Update metric.py

--- utils/metric.py
+++ utils/metric.py
@@ -6,11 +6,12 @@ import numpy as np
 import logging
 from torch.nn.parameter import Parameter
 from torch.autograd import Variable
+
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-class EMA():
+class EMA:
     def __init__(self, decay=0.999):
         self.decay = decay
         self.shadow = {}
@@ -23,12 +24,17 @@ class EMA():
 
     def update(self, name, x):
         assert name in self.shadow
-        new_average = (1.0 - self.decay) * x.cpu().detach() + self.decay * self.shadow[name]
+        new_average = (1.0 - self.decay) * x.cpu().detach() + self.decay * self.shadow[
+            name
+        ]
         self.shadow[name] = new_average.clone()
+
+
 def l2norm(x):
-  """L2-normalize columns of x"""
-  norm = torch.pow(x, 2).sum(dim=-1, keepdim=True).sqrt()
-  return torch.div(x, norm)
+    """L2-normalize columns of x"""
+    norm = torch.pow(x, 2).sum(dim=-1, keepdim=True).sqrt()
+    return torch.div(x, norm)
+
 
 def pairwise_distance(A, B):
     """
@@ -43,7 +49,6 @@ def pairwise_distance(A, B):
     distance = A_square + B_square.t() - 2 * torch.matmul(A, B.t())
 
     return distance
-
 
 
 def one_hot_coding(index, k):
@@ -79,7 +84,7 @@ def constraints(features, labels):
     con_loss = AverageMeter()
     index_dict = {k.item() for k in labels}
     for index in index_dict:
-        labels_mask = (labels == index)
+        labels_mask = labels == index
         feas = torch.masked_select(features, labels_mask)
         feas = feas.view(-1, features.shape[1])
         distance = pairwise_distance(feas, feas)
@@ -104,10 +109,12 @@ def constraints_loss(data_loader, network, args):
             images = images.cuda()
             captions = captions.cuda()
             interval = images.shape[0]
-            image_embeddings, text_embeddings = network(images, captions, captions_length)
-            images_bank[index: index + interval] = image_embeddings
-            text_bank[index: index + interval] = text_embeddings
-            labels_bank[index: index + interval] = labels
+            image_embeddings, text_embeddings = network(
+                images, captions, captions_length
+            )
+            images_bank[index : index + interval] = image_embeddings
+            text_bank[index : index + interval] = text_embeddings
+            labels_bank[index : index + interval] = labels
             index = index + interval
         images_bank = images_bank[:index]
         text_bank = text_bank[:index]
@@ -130,8 +137,8 @@ class Loss(nn.Module):
         self.num_classes = args.num_classes
         if args.resume:
             checkpoint = torch.load(args.model_path)
-            self.W = Parameter(checkpoint['W'])
-            print('=========> Loading in parameter W from pretrained models')
+            self.W = Parameter(checkpoint["W"])
+            print("=========> Loading in parameter W from pretrained models")
         else:
             self.W = Parameter(torch.randn(args.feature_size, args.num_classes))
             self.init_weight()
@@ -142,14 +149,16 @@ class Loss(nn.Module):
     def diversity_loss(self, x):
         x = l2norm(x)  # Columns of x MUST be l2-normalized
         gram_x = x.bmm(x.transpose(1, 2))
-        I = torch.autograd.Variable((torch.eye(x.size(1)) > 0.5).repeat(gram_x.size(0), 1, 1))
+        I = torch.autograd.Variable(
+            (torch.eye(x.size(1)) > 0.5).repeat(gram_x.size(0), 1, 1)
+        )
         if torch.cuda.is_available():
             I = I.cuda()
         gram_x.masked_fill_(I, 0.0)
         loss = torch.stack([torch.norm(g, p=2) for g in gram_x]) / (x.size(1) ** 2)
         return loss.mean()
 
-    def diversity_loss2(self,features):
+    def diversity_loss2(self, features):
         batch_size, num_parts, num_feat = features.size()
         # 向量单位化
         features_norm = F.normalize(features, dim=2)
@@ -157,8 +166,11 @@ class Loss(nn.Module):
         feaures_t = features_norm.transpose(1, 2)
         loss_tentor = torch.matmul(features_norm, feaures_t)
         # 计算损失
-        diversity_loss = (torch.sum(loss_tentor) - 1 * batch_size * num_parts) / (num_parts * (num_parts - 1))
+        diversity_loss = (torch.sum(loss_tentor) - 1 * batch_size * num_parts) / (
+            num_parts * (num_parts - 1)
+        )
         return diversity_loss
+
     def compute_cmpc_loss(self, image_embeddings, text_embeddings, labels):
         """
         Cross-Modal Projection Classfication loss(CMPC)
@@ -167,24 +179,28 @@ class Loss(nn.Module):
         :param labels: Tensor with dtype torch.int32
         :return:
         """
-        criterion = nn.CrossEntropyLoss(reduction='mean')
+        criterion = nn.CrossEntropyLoss(reduction="mean")
         self.W_norm = self.W / self.W.norm(dim=0)
         # labels_onehot = one_hot_coding(labels, self.num_classes).float()
         image_norm = image_embeddings / image_embeddings.norm(dim=1, keepdim=True)
         text_norm = text_embeddings / text_embeddings.norm(dim=1, keepdim=True)
 
-        image_proj_text = torch.sum(image_embeddings * text_norm, dim=1, keepdim=True) * text_norm
-        text_proj_image = torch.sum(text_embeddings * image_norm, dim=1, keepdim=True) * image_norm
+        image_proj_text = (
+            torch.sum(image_embeddings * text_norm, dim=1, keepdim=True) * text_norm
+        )
+        text_proj_image = (
+            torch.sum(text_embeddings * image_norm, dim=1, keepdim=True) * image_norm
+        )
 
         image_logits = torch.matmul(image_proj_text, self.W_norm)
         text_logits = torch.matmul(text_proj_image, self.W_norm)
 
         # labels_one_hot = one_hot_coding(labels, num_classes)
-        '''
+        """
         ipt_loss = criterion(input=image_logits, target=labels)
         tpi_loss = criterion(input=text_logits, target=labels)
         cmpc_loss = ipt_loss + tpi_loss
-        '''
+        """
         cmpc_loss = criterion(image_logits, labels) + criterion(text_logits, labels)
 
         image_pred = torch.argmax(image_logits, dim=1)
@@ -211,7 +227,7 @@ class Loss(nn.Module):
         batch_size = image_embeddings.shape[0]
         labels_reshape = torch.reshape(labels, (batch_size, 1))
         labels_dist = labels_reshape - labels_reshape.t()
-        labels_mask = (labels_dist == 0)
+        labels_mask = labels_dist == 0
 
         image_norm = image_embeddings / image_embeddings.norm(dim=1, keepdim=True)
         text_norm = text_embeddings / text_embeddings.norm(dim=1, keepdim=True)
@@ -223,13 +239,21 @@ class Loss(nn.Module):
 
         i2t_pred = F.softmax(image_proj_text, dim=1)
         # i2t_loss = i2t_pred * torch.log((i2t_pred + self.epsilon)/ (labels_mask_norm + self.epsilon))
-        i2t_loss = i2t_pred * (F.log_softmax(image_proj_text, dim=1) - torch.log(labels_mask_norm + self.epsilon))
+        i2t_loss = i2t_pred * (
+            F.log_softmax(image_proj_text, dim=1)
+            - torch.log(labels_mask_norm + self.epsilon)
+        )
 
         t2i_pred = F.softmax(text_proj_image, dim=1)
         # t2i_loss = t2i_pred * torch.log((t2i_pred + self.epsilon)/ (labels_mask_norm + self.epsilon))
-        t2i_loss = t2i_pred * (F.log_softmax(text_proj_image, dim=1) - torch.log(labels_mask_norm + self.epsilon))
+        t2i_loss = t2i_pred * (
+            F.log_softmax(text_proj_image, dim=1)
+            - torch.log(labels_mask_norm + self.epsilon)
+        )
 
-        cmpm_loss = torch.mean(torch.sum(i2t_loss, dim=1)) + torch.mean(torch.sum(t2i_loss, dim=1))
+        cmpm_loss = torch.mean(torch.sum(i2t_loss, dim=1)) + torch.mean(
+            torch.sum(t2i_loss, dim=1)
+        )
 
         sim_cos = torch.matmul(image_norm, text_norm.t())
 
@@ -238,7 +262,9 @@ class Loss(nn.Module):
 
         return cmpm_loss, pos_avg_sim, neg_avg_sim
 
-    def forward(self, image_embeddings, text_embeddings,img_f,text_f,labels,lambda_diversity):
+    def forward(
+        self, image_embeddings, text_embeddings, img_f, text_f, labels, lambda_diversity
+    ):
         # self.diversity_loss2(img_f)
         cmpm_loss = 0.0
         cmpc_loss = 0.0
@@ -247,26 +273,40 @@ class Loss(nn.Module):
         neg_avg_sim = 0.0
         pos_avg_sim = 0.0
         if self.CMPM:
-            cmpm_loss=0
+            cmpm_loss = 0
             for i in range(len(image_embeddings)):
-                cmpm_loss1, pos_avg_sim, neg_avg_sim = self.compute_cmpm_loss(image_embeddings[i], text_embeddings[i], labels)
+                cmpm_loss1, pos_avg_sim, neg_avg_sim = self.compute_cmpm_loss(
+                    image_embeddings[i], text_embeddings[i], labels
+                )
                 # if i==0:
-                cmpm_loss+=cmpm_loss1
+                cmpm_loss += cmpm_loss1
                 # else:
                 #     cmpm_loss += cmpm_loss1*1.0/(len(image_embeddings)-1)
 
         if self.CMPC:
-            cmpc_loss=0
+            cmpc_loss = 0
             for i in range(len(image_embeddings)):
-                cmpc_loss1, image_precision, text_precision = self.compute_cmpc_loss(image_embeddings[i], text_embeddings[i], labels)
+                cmpc_loss1, image_precision, text_precision = self.compute_cmpc_loss(
+                    image_embeddings[i], text_embeddings[i], labels
+                )
                 # if i==0:
-                cmpc_loss+=cmpc_loss1
+                cmpc_loss += cmpc_loss1
                 # else:
                 #     cmpc_loss += cmpc_loss1*1.0/(len(image_embeddings)-1)
 
         loss = cmpm_loss + cmpc_loss
-        loss+=lambda_diversity*(self.diversity_loss(img_f[:,1:])+self.diversity_loss(text_f[:,1:]))
-        return cmpm_loss, cmpc_loss, loss, image_precision, text_precision, pos_avg_sim, neg_avg_sim
+        loss += lambda_diversity * (
+            self.diversity_loss(img_f) + self.diversity_loss(text_f)
+        )
+        return (
+            cmpm_loss,
+            cmpc_loss,
+            loss,
+            image_precision,
+            text_precision,
+            pos_avg_sim,
+            neg_avg_sim,
+        )
 
 
 class AverageMeter(object):
@@ -277,11 +317,13 @@ class AverageMeter(object):
 
     def __init__(self):
         self.reset()
+
     def reset(self):
         self.val = 0
         self.avg = 0
         self.sum = 0
         self.count = 0
+
     def update(self, val, n=1):
         self.val = val
         self.sum += n * val
@@ -289,18 +331,38 @@ class AverageMeter(object):
         self.avg = self.sum / self.count
 
 
+# def compute_topk(query, gallery, target_query, target_gallery, k=[1, 10], reverse=False):
+#     result = []
+#     query0 = query[0] / query[0].norm(dim=1, keepdim=True)
+#     gallery0 = gallery[0] / gallery[0].norm(dim=1, keepdim=True)
+#     sim_cosine = torch.matmul(query0, gallery0.t())
+#     print(sim_cosine.shape)
+#     score = torch.zeros((sim_cosine.shape))
+#     for i in range(query.size(0)):
+#         query0 = query[i] / query[i].norm(dim=1, keepdim=True)
+#         gallery0 = gallery[i] / gallery[i].norm(dim=1, keepdim=True)
+#         sim_cosine = torch.matmul(query0, gallery0.t())
+#         score+=sim_cosine
+#     result.extend(topk(score, target_gallery, target_query, k, dim=1))
+#     if reverse:
+#         result.extend(topk(score, target_query, target_gallery, k, dim=0))
+#     return result,score
 
-def compute_topk(query, gallery, target_query, target_gallery, k=[1, 10], reverse=False):
+
+def compute_topk(
+    query, gallery, target_query, target_gallery, k=[1, 10], reverse=False
+):
     result = []
-    test_query=query.reshape(query.size(0),-1)
-    test_gallery=gallery.reshape(gallery.size(0),-1)
-    query_norm=test_query / test_query.norm(dim=1,keepdim=True)
-    gallery_norm=test_gallery / test_gallery.norm(dim=1,keepdim=True)
-    score=torch.matmul(query_norm,gallery_norm.t())
+    test_query = query.reshape(query.size(0), -1)
+    test_gallery = gallery.reshape(gallery.size(0), -1)
+    query_norm = test_query / test_query.norm(dim=1, keepdim=True)
+    gallery_norm = test_gallery / test_gallery.norm(dim=1, keepdim=True)
+    score = torch.matmul(query_norm, gallery_norm.t())
     result.extend(topk(score, target_gallery, target_query, k, dim=1))
     if reverse:
         result.extend(topk(score, target_query, target_gallery, k, dim=0))
-    return result,score
+    return result, score
+
 
 def topk(sim, target_gallery, target_query, k=[1, 10], dim=1):
     result = []
